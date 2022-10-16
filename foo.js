f142019e5b52d87240504954303483fdf231ab1f
livingfactory <111710162+livingfactory@users.noreply.github.com>
2022-10-16 14:46:13 -0400

    Install moment
    Installs moment and moment timezone

--- foo.js
+++ foo.js
@@ -1,24 +1,9 @@
-   /*
-   
+/*
+
    whoa!    Hold your horses! This is badly formatted code.
-                       
-   
+
 */
 
+const message = 'Hello World';
 
-
-       var message =      "Hello World"
-
-
-
-
-
-
-
-
-console.log(message)            // this should not show as an error
-
-
-
-
-
+console.log(message); // this should not show as an error
